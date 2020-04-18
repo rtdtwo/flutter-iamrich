a5cc5bb3c2f7c125bb97be69367d607a6f310d88
Rishabh Tatiraju <tatiraju.rishabh@gmail.com>
2020-04-18 12:22:41 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[700],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/money.jpg'),
+            height: double.infinity,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+        )),
+  ));
+}
