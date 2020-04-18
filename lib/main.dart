afd7b65f7b1983dc94c663a3acef932d7d07eb68
Rishabh Tatiraju <tatiraju.rishabh@gmail.com>
2020-04-18 13:18:20 +0530

    Converted to a stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,26 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-        appBar: AppBar(
-          title: Text('I Am Rich'),
-          backgroundColor: Colors.blueGrey[700],
-        ),
-        body: Center(
-          child: Image(
-            image: AssetImage('images/money.jpg'),
-            height: double.infinity,
-            width: double.infinity,
-            fit: BoxFit.cover,
+  runApp(IAmRichApp());
+}
+
+class IAmRichApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('I Am Rich'),
+            backgroundColor: Colors.blueGrey[900],
           ),
-        )),
-  ));
+          body: Center(
+            child: Image(
+              image: AssetImage('images/money.jpg'),
+              height: double.infinity,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          )),
+    );
+  }
 }
